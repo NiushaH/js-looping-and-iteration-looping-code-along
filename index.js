664a9597c965acffe572308f70d63027292e69b3
NiushaH <35556570+NiushaH@users.noreply.github.com>
2022-09-04 16:23:19 -0600

    commit tree 42a51e064fdf420300f87e29f84043772bfa5cf7

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solutions in this file
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}
+
+
+// function countDown (numberfromuserinput) {
+//   while (numberfromuserinput > 0) {
+//     console.log(numberfromuserinput);
+//     numberfromuserinput -= 1;
+//   }
+// console.log(numberfromuserinput(10));
+// }
+
+
+// function countdown(i) {
+//     while(i<=0)  {  
+//         return i;
+//     return i + " " + countdown(--i);
+//     }
+//   console.log(countdown(5));
+// }
+
+
+// function countdown(i); {
+//   while (countdown > 0){
+//       countdown--;
+//       console.log(countdown);
+//   }
+//   console.log(countdown(15));
+// }
+// console.log("Blastoff");
